61fc9e3b0e286539f9949cc00fae89438a0c0bac
Kirk Wang <k3wang@gmail.com>
2025-05-02 05:51:43 -0700

    🧹 Change a few labels
    Change a few labels from collections to sets and repositories to series.
    Also, changing the path of the view all sets to a specific faceted
    search instead.

--- app/helpers/arclight_helper_decorator.rb
+++ app/helpers/arclight_helper_decorator.rb
@@ -8,6 +8,14 @@ module ArclightHelperDecorator
   def show_sidebar_classes
     "col-lg-3 order-1 collection-sidebar"
   end
+
+  def sets_path
+    search_action_url(
+      f: {
+        Category: [ "Set" ]
+      }
+    )
+  end
 end
 
 ArclightHelper.prepend(ArclightHelperDecorator)

9ced9ed286698b3c030ea1d359a4a1c80f9423b4
Kirk Wang <k3wang@gmail.com>
2025-05-03 11:38:25 -0700

    🎁 Show thumbnails on catalog search
    This will add small thumbnail previews on the catalog search results.

---
+++ app/components/arclight/search_result_component_decorator.rb
@@ -0,0 +1,14 @@
+# OVERRIDE Arclight v2.0.0.alpha to add the thumbnail image instead of a SVG icon
+
+module Arclight
+  module SearchResultComponentDecorator
+    def icon
+      link_to(
+        image_tag(@document.icon_url, width: "100%", alt: "#{@document.normalized_title} thumbnail", loading: "lazy"),
+        solr_document_path(@document.id)
+      )
+    end
+  end
+end
+
+Arclight::SearchResultComponent.prepend(Arclight::SearchResultComponentDecorator)

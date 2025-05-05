3e32dd32a0698edf7838d10fddf954c023b1ed5b
Kirk Wang <k3wang@gmail.com>
2025-05-04 19:51:14 -0700

    ♻️ Refactor Repository logic to the model
    Overriding the .all and .find method and moving the logic from the
    controller to the model makes a lot more sense.  Needed to override a
    few methods in the `SolrDocument` and `Arclight::Repository` to make it
    work.

--- app/controllers/arclight/repositories_controller_decorator.rb
+++ app/controllers/arclight/repositories_controller_decorator.rb
@@ -3,24 +3,14 @@
 module Arclight
   module RepositoriesControllerDecorator
     def index
-      @repositories = @repositories.sort_by(&:final_set_release_date).reverse
+      @repositories = Arclight::Repository.all.sort_by(&:final_set_release_date).reverse
     end
 
     def show
-      @repository = @repositories.find { |repository| repository.name == params[:id] }
+      @repository = Arclight::Repository.find(params[:id])
       @collections = @repository.documents
-    end
-
-    private
-
-    def set_repositories
-      search_service = Blacklight.repository_class.new(blacklight_config)
-      documents = search_service.search(q: "series_ssim:*", fq: "level_ssm:\"collection\"", rows: 10_000).documents
-
-      @repositories = documents.group_by(&:series).map { |name, documents| Arclight::Repository.new(name, documents) }
     end
   end
 end
 
 Arclight::RepositoriesController.prepend(Arclight::RepositoriesControllerDecorator)
-Arclight::RepositoriesController.before_action :set_repositories, only: [ :index, :show ]

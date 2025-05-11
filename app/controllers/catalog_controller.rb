7ae6a928dac06f120a11041292b44da0ef191123
Kirk Wang <k3wang@gmail.com>
2025-05-11 13:02:41 -0700

    🐛 Fix logic for pokemon search
    When we do an all fields search it now returns not just the card with
    the Pokemon name because sometimes attack/ability text would reference
    the name of the Pokemon.  I tweaked it so that it now searches for the
    Pokemon name.  Also slowly adding back search options, starting with the
    "Card Name".

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -236,6 +236,14 @@ class CatalogController < ApplicationController
       }
     end
 
+    config.add_search_field "normalized_title_ssm", label: "Card Name" do |field|
+      field.qt = "search"
+      field.solr_parameters = {
+        qf:  "title_tesim",
+        pf:  "title_tesim"
+      }
+    end
+
     # Field-based searches. We have registered handlers in the Solr configuration
     # so we have Blacklight use the `qt` parameter to invoke them
     # config.add_search_field "keyword", label: "Keyword" do |field|

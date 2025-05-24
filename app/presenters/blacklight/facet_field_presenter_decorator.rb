b85feee13e442254e4fed42093441d1eb032e3a0
Kirk Wang <k3wang@gmail.com>
2025-05-24 09:38:14 -0700

    ⚙️ Remove facet limit on advanced search form
    Since on the advanced search form we have checkboxes instead, I've
    decided not limit it because if you click the 'more' link to pop up the
    modal, it doesn't actually work like the facet on the form because it
    shows the normal facet modal which doesn't have checkboxes.

--- app/presenters/blacklight/facet_field_presenter_decorator.rb
+++ app/presenters/blacklight/facet_field_presenter_decorator.rb
@@ -1,9 +1,17 @@
 # OVERRIDE Blacklight 8.11.0 to keep a facet card open if there are excluded values
+#   and to not show the 'more' link on the advanced search form since we're not
+#   limiting it there
 
 module Blacklight
   module FacetFieldPresenterDecorator
     def collapsed?
       super && !search_state.params[:f]&.keys&.include?("-#{facet_field.key}")
+    end
+
+    def facet_limit
+      return if view_context.params[:action] == 'advanced_search'
+
+      super
     end
   end
 end

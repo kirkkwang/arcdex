bcbd82105329aa29bfaf58b4d41ca1f27ba90581
Kirk Wang <kirk.wang@scientist.com>
2025-05-23 22:56:45 -0700

    💄 Add an icon to advanced search link
    This will add a magnifying glass with a plus icon to the advanced search
    link (that looks like a button).  Had to move the element a little to
    make it easier to style.

---
+++ app/components/arcdex/advanced_search_link_component.rb
@@ -0,0 +1,17 @@
+module Arcdex
+  class AdvancedSearchLinkComponent < ::Blacklight::Component
+    def initialize(text:, id:, url:)
+      @text = text
+      @id = id
+      @url = url
+    end
+
+    def call
+      tag.a(href: @url, class: 'advanced_search btn btn-secondary text-nowrap') do
+        tag.span(t('blacklight.advanced_search.more_options'),
+                class: 'visually-hidden-sm me-sm-1 submit-search-text') +
+          render(Arcdex::Icons::AdvancedSearchComponent.new)
+      end
+    end
+  end
+end

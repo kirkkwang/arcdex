e7daafd48f5b7be2ecad3a1a63b1a01e2ad0bda3
Kirk Wang <k3wang@gmail.com>
2025-08-13 16:11:56 -0700

    🐛 Fix undefined `ViewComponent::Version`
    Not sure why it's happening but `ViewComponent::Version` was not loading
    correctly so I'm adding an initializer to require it.

---
+++ config/initializers/view_component.rb
@@ -0,0 +1,3 @@
+# Load ViewComponent::Version because we're crashing in blacklight
+# Ref: https://github.com/projectblacklight/blacklight/blob/8f3a9c60bb2f97a0554b2cefb82bd9c79dfa6c4f/lib/blacklight/component.rb#L14
+require 'view_component/version'

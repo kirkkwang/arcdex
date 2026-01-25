869f853592ac46f400155cb0fa919c07efadaf0a
Kirk Wang <k3wang@gmail.com>
2026-01-24 23:03:40 -0800

    ⚙️ Add a limit to rack attack
    Trying to see if this will help with the bots that get through.

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -1,4 +1,9 @@
 class Rack::Attack
+  # Throttle requests to /catalog by IP address
+  throttle('catalog/ip', limit: 100, period: 5.minutes) do |req|
+    req.ip if req.path.start_with?('/catalog')
+  end
+
   # Block all bots from /catalog by user agent
   blocklist('block bots from catalog') do |req|
     req.path.start_with?('/catalog') &&
